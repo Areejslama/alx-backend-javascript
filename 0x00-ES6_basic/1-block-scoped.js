ff23d97f041a03c71714bf47750874068cfd322d
Areejslama <areegahmed449@gmail.com>
2024-07-23 00:12:11 +0300

    1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-   const taskInblock = false;
-   const task2Inblock = true;
-  
-  return [taskInblock, task2Inblock];
+    const taskInblock = false;
+    const task2Inblock = true;
 
+    return [taskInblock, task2Inblock];
   }
 
   return [task, task2];
