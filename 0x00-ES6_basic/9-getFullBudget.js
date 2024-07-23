3e7add5ac73dbd86bc2e5f46130b58816fde5c7e
Areejslama <areegahmed449@gmail.com>
2024-07-23 14:51:32 +0300

    9

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,13 +1,16 @@
-import getBudgetObject from './7-getBudgetObject';
+import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     budget,
-    getIncomeInDollars: () => `$${budget.income}`,
-    getIncomeInEuros: () => `${budget.income} euros`,
+    getIncomeInDollars: (income) => {
+      return `$${budget.income}`;
+    },
+    getIncomeInEuros: (income) => {
+      return `${budget.income} euros`;
+    },
   };
 
   return fullBudget;
 }
-
