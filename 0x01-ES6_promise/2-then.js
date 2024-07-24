8881e52594a2010b19209a3d354bb1f4ab087e01
Areejslama <areegahmed449@gmail.com>
2024-07-24 23:27:16 +0300

    2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({status: 200, body: 'Success'}))
-    .catch(() => (Error()))
+    .then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => Error())
     .finally(() => console.log('Got a response from the API'));
 }
 export default handleResponseFromAPI;
