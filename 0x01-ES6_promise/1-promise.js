b4c76e5a5e14a064ece706a3cccea5342e39844b
Areejslama <areegahmed449@gmail.com>
2024-07-24 21:40:57 +0300

    1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,13 @@
 function getResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success === true) {
-      const code = 200;
-      const body = 'success';
-      resolve({ code, body });
-    } else {
+      const result = {
+	      status : 200,
+	      body: 'Success'
+      };
+      resolve(result);
+    }
+	  else {
       reject(Error('The fake API is not working currently'));
     }
   });
