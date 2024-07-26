218fb753a0a8fd642ffb1f2a0b82cd3621765c95
Areejslama <areegahmed449@gmail.com>
2024-07-26 17:08:58 +0300

    7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,20 +1,24 @@
 class Airport {
-	constructor(name, code) {
-		this._name = name;
-		this._code = code;
-	}
-	
-	get name() {
-		return this._name;
-	}
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
 
-	get code() {
-		return this._code;
-	}
+  get name() {
+    return this._name;
+  }
 
-	toString() {
-		return `[${this._code}]`;
-	}
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return `[${this._code}]`;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
 }
 
 export default Airport;
