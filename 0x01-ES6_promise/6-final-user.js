300b771b2954c2c55d80d099b6a08aba87d0b5d9
Areejslama <areegahmed449@gmail.com>
2024-07-27 00:45:25 +0300

    6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const file = uploadPhoto(fileName);
+  const promises = [user, file];
+
+  return Promise.allSettled(promises)
+    .then((results) => results.map((results) => ({
+      status: results.status,
+      value: results.value || results.reason,
+    })));
+}
+
+export default handleProfileSignup;
