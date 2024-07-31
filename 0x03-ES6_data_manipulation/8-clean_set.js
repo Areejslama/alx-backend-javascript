fec561c6c10dfd34e9093355b85cc1f8438fba08
Areejslama <areegahmed449@gmail.com>
2024-07-31 04:16:48 +0300

    8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,11 @@
 export default function cleanSet(set, startString) {
-	const result = [...set]
-    .filter(value => typeof value === 'string' && value.startsWith(startString))
-    .map(value => value.slice(startString.length))
+  if (typeof startString !== 'string') {
+    throw Error('the argument must be a string');
+  }
+  const result = [...set]
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
     .join('-');
 
-	return result;
+  return result;
 }
