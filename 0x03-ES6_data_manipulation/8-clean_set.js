ec21615e4afc8280af6d893af53d4b3c9873bdd0
Areejslama <areegahmed449@gmail.com>
2024-07-31 03:04:47 +0300

    8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,6 @@
 export default function cleanSet(set, startString) {
 	const result = [...set]
         .filter(value => value.startsWith(startString))
-        .map(value => value.slice(startString.length))
         .join('-');
 
     return result;
