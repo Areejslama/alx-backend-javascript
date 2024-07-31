33f09bb08d3bb381073ba8f879a754e52e6c5918
Areejslama <areegahmed449@gmail.com>
2024-07-31 04:21:54 +0300

    8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,6 @@
 export default function cleanSet(set, startString) {
-  if (!(set instanceof Set)) {
-        throw Error('Invalid input: The first argument must be a Set');
-    }
-
-    if (typeof startString !== 'string') {
-        throw Error('Invalid input: The second argument must be a string');
-    }
-
   const result = [...set]
-    .filter((value) => typeof value === 'string' && value.startsWith(startString))
+    .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
     .join('-');
 
