efc0a15383e18f9cc08f4b68e21eea879181e247
Areejslama <areegahmed449@gmail.com>
2024-09-18 01:13:51 +0300

    stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  const name = data;
+  console.log(`Your name is: ${name}`);
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
