fc01d8fba3da84f16071a8d7f7915d66197e9ffd
Areejslama <areegahmed449@gmail.com>
2024-09-20 22:27:12 +0300

    0

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', function() {
+	it('should return rounded number', function() {
+		const result = calculateNumber(1, 3);
+		assert.strictEqual(result, 4);
+	});
+        
+	it('should return rounded number', function() {
+                const result = calculateNumber(1, 3.7);
+                assert.strictEqual(result, 5);
+	});
+        
+	it('should return rounded number', function() {
+                const result = calculateNumber(1.2, 3.7);
+                assert.strictEqual(result, 5);
+	});
+
+	it('should return rounded number', function() {
+                const result = calculateNumber(1.5, 3.7);
+                assert.strictEqual(result, 6);
+	});
+});
