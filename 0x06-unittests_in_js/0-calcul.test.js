74ef46867c8c833531f3151471769aa24fed6a3c
Areejslama <areegahmed449@gmail.com>
2024-09-20 22:30:24 +0300

    0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -21,4 +21,9 @@ describe('calculateNumber', function() {
                 const result = calculateNumber(1.5, 3.7);
                 assert.strictEqual(result, 6);
 	});
+
+	 it('should return rounded number', function() {
+                const result = calculateNumber(-1.4, -2.6);
+                assert.strictEqual(result, -4);
+	});
 });
