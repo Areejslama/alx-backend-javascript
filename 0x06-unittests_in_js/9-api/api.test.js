53ea4b25dbdb4a091e1e299fc59a7f9ad0b18397
Areejslama <areegahmed449@gmail.com>
2024-09-21 19:29:40 +0300

    9

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,35 +1,36 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe('Basic Integration testing', function() {
+describe('Cart API Integration Tests', function() {
+    const host = 'http://localhost:7865';
+
     it('should return welcome message', function(done) {
-        const host = 'http://localhost:7865';
         request.get(`${host}/`, (err, res, body) => {
             expect(res.statusCode).to.equal(200);
             expect(body).to.equal('Welcome to the payment system');
             done();
         });
     });
-});
 
-it('should return correct id', function(done) {
+    it('should return correct id for a valid number', function(done) {
         request.get(`${host}/cart/10`, (err, res, body) => {
             expect(res.statusCode).to.equal(200);
             expect(body).to.equal('Payment methods for cart 10');
-	    done();
+            done();
+        });
     });
-});
 
-it('should return correct id', function(done) {
+    it('should return 404 for a negative number', function(done) {
         request.get(`${host}/cart/-5`, (err, res, body) => {
-		expect(res.statusCode).to.equal(404);
-		done();
-});
-});
+            expect(res.statusCode).to.equal(404);
+            done();
+        });
+    });
 
-it('should return correct id', function(done) {
+    it('should return 404 for a non-numeric id', function(done) {
         request.get(`${host}/cart/hfh4551`, (err, res, body) => {
-                expect(res.statusCode).to.equal(404);
-                done();
-});
+            expect(res.statusCode).to.equal(404);
+            done();
+        });
+    });
 });
