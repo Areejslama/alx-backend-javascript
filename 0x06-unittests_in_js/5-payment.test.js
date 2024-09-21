93fe0a238c67fa5cefc3e6a4dfd0bc16f427c54a
Areejslama <areegahmed449@gmail.com>
2024-09-21 04:53:09 +0300

    hook

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -18,9 +18,12 @@ describe('sendPaymentRequestToAPI', function() {
 
         expect(spy.calledWith('The total is: 120')).to.be.true;
         expect(spy.calledOnce).to.be.true;
+
+     it('should log the total correctly', function() {
 	
 	 sendPaymentRequestToApi(10, 10);
 	 expect(spy.calledWith('The total is: 20')).to.be.true;
 	 expect(spy.calledOnce).to.be.true;
+    });
     });
 });
