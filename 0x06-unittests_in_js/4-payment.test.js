45e98df71a957fb68a1628ac5db04b069411d036
Areejslama <areegahmed449@gmail.com>
2024-09-21 04:31:41 +0300

    4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,15 +4,17 @@ const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('Payment Tests', function() {
-    it('should call the function', function() {
-	const spy = sinon.spy(console);
-        const stubs = sinon.spy(Utils, 'calculateNumber').returns(10);
+    it('should call Utils.calculateNumber and log the total', function() {
+        const consoleSpy = sinon.spy(console, 'log');
+        const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+
         sendPaymentRequestToApi(100, 20);
-        expect(stubs.calledWith('SUM', 100, 20)).to.be.true;
-        expect(stubs.callcount).to.equal(1);
-	expect(spy.log.calledWith('The total is: 10')).to.be.true;
-        expect(spy.log.callCount).to.equal(1);
-	spy.restore();
-	stubs.restore();
+
+        expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+	expect(stub.callCount).to.equal(1);
+        expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
+        expect(consoleSpy.callCount).to.equal(1);
+        stub.restore();
+        consoleSpy.restore();
     });
 });
